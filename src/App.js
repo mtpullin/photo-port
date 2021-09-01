cbed13a0508a0c3637b56ad9f63607b9af5fcab6
mtpullin <mtpullin@gmail.com>
2021-09-01 08:31:01 -0400

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import About from './components/about';
-import Nav from './components/Nav'
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
@@ -13,17 +12,19 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ])
-  const [currentCategory, setCurrentCategory] = useState(categories[0])
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
