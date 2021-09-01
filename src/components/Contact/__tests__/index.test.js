5944f12cf590bc12bc4c41c1801a3dc2f6dab4eb
mtpullin <mtpullin@gmail.com>
2021-09-01 09:49:58 -0400

    finished user submission form

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Contact from '../../Contact'
+
+afterEach (cleanup)
+
+describe('About component renders', () => {
+    it('renders', () => {
+      render(<Contact />);
+    });
+    
+    it('matches snapshot DOM node structure', () => {
+      const { asFragment } = render(<Contact />);
+      
+      expect(asFragment()).toMatchSnapshot();
+    });
+  })
